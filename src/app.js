21bed56dd6c9367e62483181296328fd19c174f1
Douglas Falcao <falcao.douglas97@gmail.com>
2021-11-26 16:35:19 -0300

    likes done

--- src/app.js
+++ src/app.js
@@ -12,22 +12,24 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const repositories = [];
+const repositories = [
+  { id: uuid(), title: "Front", url: "http://facebook.com", techs: "React", likes: 20 },
+  { id: uuid(), title: "Backend", url: "http://facebook.com", techs: "Node", likes: 30 }
+];
 
 app.get("/repositories", (request, response) => {
-  const { title, url, techs, likes } = request.body;
-  repositoriesFormat = { id: uuid(), title, url, techs, likes }
-  if(likes.request.body != 0){
-    likes.response.body = 0;
-  }
-  console.log(repositoriesFormat)
-
-
-  return response.json(repositoriesFormat)
+  return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, tech, url } = request.body;
+
+  const repository = {
+    id: uuid(), title, tech, likes: 0, url
+  }
+
+  repositories.push(repository)
+  return response.json(repository)
 });
 
 app.put("/repositories/:id", (request, response) => {
